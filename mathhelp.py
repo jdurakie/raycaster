9a47351ca592e7a0154cbc8e51735c2d69244517
Jan Durakiewicz <jan_durakiewicz@trimble.com>
2018-09-11 17:56:52 -0600

    Updates to profiling

--- mathhelp.py
+++ mathhelp.py
@@ -207,49 +207,49 @@ def lineImpactsSphere(sphere, line):
 #         return None
 #     return Q
 
-# V3
-def triangleLineIntersect(triangle, line):
-    # Based on handout by Brian Curless:
-    # https://courses.cs.washington.edu/courses/csep557/10au/lectures/triangle_intersection.pdf
-    #print(triangle)
-    #figure out the normal of the triangle's plane
-    tA = triangle.A
-    tB = triangle.B
-    tC = triangle.C
-    n = c_mathhelp.normalize(triangle.normal())
-    #find p (line origin) and dirv (line direction vector)
-    P = line.start
-    end = line.end
-    dirv = c_mathhelp.normalize(c_mathhelp.subtractPoint(P, end))
-    #solve for d (righthand of plane equation)
-    d = c_mathhelp.dotproduct(n, tA)
-    #solve for t : vector magnitude coeficcient in ray equation
-    #             R(t) = P + t * dirv
-    top = d - c_mathhelp.dotproduct(n, P)
-    bottom = c_mathhelp.dotproduct(n, dirv)
-    #if the bottom is 0, the line is parallel to the triangle
-    #(triangle normal and dirv are perpendicular)
-    if areEqualFloats(bottom, 0.0):
-        return None
-    t =  top / bottom
-    # plug T into the equation for the ray to get ray-plane intersection
-    Q = c_mathhelp.addPoint(P, c_mathhelp.multiplyPointByScalar(dirv, t))
-    #If the intersection is behind the screen, ignore it
-    #figure out whether the point is inside the triangle or not
-    # by checking if it's 'to the right' of all the triangle's edges
-    AB = c_mathhelp.subtractPoint(tB, tA)
-    AQ = c_mathhelp.subtractPoint(Q, tA)
-    ABcross = c_mathhelp.dotproduct(cross(AB, AQ), n)
-    if ABcross < 0:
-        return None
-    BC = c_mathhelp.subtractPoint(tC, tB)
-    BQ = c_mathhelp.subtractPoint(Q, tB)
-    BCcross = c_mathhelp.dotproduct(cross(BC, BQ), n)
-    if BCcross < 0:
-        return None
-    CA = c_mathhelp.subtractPoint(tA, tC)
-    CQ = c_mathhelp.subtractPoint(Q, tC)
-    CAcross = c_mathhelp.dotproduct(cross(CA, CQ), n)
-    if CAcross < 0:
-        return None
-    return Q
+# # V3
+# def triangleLineIntersect(triangle, line):
+#     # Based on handout by Brian Curless:
+#     # https://courses.cs.washington.edu/courses/csep557/10au/lectures/triangle_intersection.pdf
+#     #print(triangle)
+#     #figure out the normal of the triangle's plane
+#     tA = triangle.A
+#     tB = triangle.B
+#     tC = triangle.C
+#     n = c_mathhelp.normalize(triangle.normal())
+#     #find p (line origin) and dirv (line direction vector)
+#     P = line.start
+#     end = line.end
+#     dirv = c_mathhelp.normalize(c_mathhelp.subtractPoint(P, end))
+#     #solve for d (righthand of plane equation)
+#     d = c_mathhelp.dotproduct(n, tA)
+#     #solve for t : vector magnitude coeficcient in ray equation
+#     #             R(t) = P + t * dirv
+#     top = d - c_mathhelp.dotproduct(n, P)
+#     bottom = c_mathhelp.dotproduct(n, dirv)
+#     #if the bottom is 0, the line is parallel to the triangle
+#     #(triangle normal and dirv are perpendicular)
+#     if areEqualFloats(bottom, 0.0):
+#         return None
+#     t =  top / bottom
+#     # plug T into the equation for the ray to get ray-plane intersection
+#     Q = c_mathhelp.addPoint(P, c_mathhelp.multiplyPointByScalar(dirv, t))
+#     #If the intersection is behind the screen, ignore it
+#     #figure out whether the point is inside the triangle or not
+#     # by checking if it's 'to the right' of all the triangle's edges
+#     AB = c_mathhelp.subtractPoint(tB, tA)
+#     AQ = c_mathhelp.subtractPoint(Q, tA)
+#     ABcross = c_mathhelp.dotproduct(cross(AB, AQ), n)
+#     if ABcross < 0:
+#         return None
+#     BC = c_mathhelp.subtractPoint(tC, tB)
+#     BQ = c_mathhelp.subtractPoint(Q, tB)
+#     BCcross = c_mathhelp.dotproduct(cross(BC, BQ), n)
+#     if BCcross < 0:
+#         return None
+#     CA = c_mathhelp.subtractPoint(tA, tC)
+#     CQ = c_mathhelp.subtractPoint(Q, tC)
+#     CAcross = c_mathhelp.dotproduct(cross(CA, CQ), n)
+#     if CAcross < 0:
+#         return None
+#     return Q
--- triangleLineIntersectProfile.py
+++ triangleLineIntersectProfile.py
@@ -1,8 +1,8 @@
 import Line
 import Triangle
-import mathhelp
+import c_mathhelp
 print('\ttriangleLineIntesect')
+l = Line.Line((0, 0, 0), (1, 0, 6))
+t = Triangle.Triangle((-1, -1, 3), (1, -1, 3), (0, 1, 3))
 for i in range(0, 100000):
-    l = Line.Line((0, 0, 0), (1, 0, 6))
-    t = Triangle.Triangle((-1, -1, 3), (1, -1, 3), (0, 1, 3))
-    mathhelp.triangleLineIntersect(t, l)
+    c_mathhelp.triangleLineIntersect(t.A, t.B, t.C, l.start, l.end)
